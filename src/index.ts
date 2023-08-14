69dd2f177b3b86d8ad8178f3187038baac700b82
Marco Falkenberg <m.falkenberg@mittwald.de>
2023-08-14 08:33:29 +0200

    Export AsyncResource class from package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./refresh.js";
 export * from "./use-promise/usePromise.js";
 export * from "./resource/getAsyncResource.js";
+export * from "./resource/AsyncResource.js";
 export * from "./resource/resourceify.js";
