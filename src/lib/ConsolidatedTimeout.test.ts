d934b477e2175e66e159db9ec7ab71fda28b360e
Marco Falkenberg <m.falkenberg@mittwald.de>
2023-09-06 14:05:48 +0200

    Make test more expressive

--- src/lib/ConsolidatedTimeout.test.ts
+++ src/lib/ConsolidatedTimeout.test.ts
@@ -15,81 +15,73 @@ afterEach((): void => {
   timeout.stop();
 });
 
+const testCallbackIsCalledAfter = (ms: number): void => {
+  const beforeCalls = callback.mock.calls.length;
+  jest.advanceTimersByTime(ms - 1);
+  expect(callback).toHaveBeenCalledTimes(beforeCalls);
+  jest.advanceTimersByTime(1);
+  expect(callback).toHaveBeenCalledTimes(beforeCalls + 1);
+};
+
+const testCallbackIsNotCalledAfter = (ms: number): void => {
+  const beforeCalls = callback.mock.calls.length;
+  jest.advanceTimersByTime(ms - 1);
+  expect(callback).toHaveBeenCalledTimes(beforeCalls);
+  jest.advanceTimersByTime(1);
+  expect(callback).toHaveBeenCalledTimes(beforeCalls);
+};
+
 test("Callback is not triggered after start when there is no timeout added", (): void => {
   timeout.start();
-  jest.advanceTimersByTime(Number.MAX_SAFE_INTEGER);
-  expect(callback).not.toHaveBeenCalled();
+  testCallbackIsNotCalledAfter(Number.MAX_SAFE_INTEGER);
 });
 
 test("Callback is triggered (only once) after timeout added after start", (): void => {
   timeout.start();
   timeout.addTimeout({ milliseconds: 1000 });
 
-  jest.advanceTimersByTime(999);
-  expect(callback).toHaveBeenCalledTimes(0);
-
-  jest.advanceTimersByTime(1);
-  expect(callback).toHaveBeenCalledTimes(1);
-
-  jest.advanceTimersByTime(Number.MAX_SAFE_INTEGER);
-  expect(callback).toHaveBeenCalledTimes(1);
+  testCallbackIsCalledAfter(1000);
+  testCallbackIsNotCalledAfter(Number.MAX_SAFE_INTEGER);
 });
 
 test("Callback is triggered after timeout added before start", (): void => {
   timeout.addTimeout({ milliseconds: 1000 });
   timeout.start();
-
-  jest.advanceTimersByTime(999);
-  expect(callback).toHaveBeenCalledTimes(0);
-
-  jest.advanceTimersByTime(1);
-  expect(callback).toHaveBeenCalledTimes(1);
+  testCallbackIsCalledAfter(1000);
 });
 
 test("Callback is triggered at minimum timeout", (): void => {
   timeout.start();
   timeout.addTimeout({ milliseconds: 1000 });
   timeout.addTimeout({ milliseconds: 500 });
-
-  jest.advanceTimersByTime(499);
-  expect(callback).toHaveBeenCalledTimes(0);
-
-  jest.advanceTimersByTime(1);
-  expect(callback).toHaveBeenCalledTimes(1);
+  testCallbackIsCalledAfter(500);
 });
 
 test("Consecutive start call restarts the timeout", (): void => {
   timeout.start();
   timeout.addTimeout({ milliseconds: 1000 });
-  jest.advanceTimersByTime(999);
+  testCallbackIsNotCalledAfter(999);
 
   timeout.start();
-  jest.advanceTimersByTime(500);
-  expect(callback).toHaveBeenCalledTimes(0);
-  jest.advanceTimersByTime(500);
-  expect(callback).toHaveBeenCalledTimes(1);
+  testCallbackIsNotCalledAfter(500);
+  testCallbackIsCalledAfter(500);
 });
 
 test("Callback is triggered when adding timeout while already running", (): void => {
   timeout.start();
 
   timeout.addTimeout({ milliseconds: 1000 });
-  jest.advanceTimersByTime(499);
+  testCallbackIsNotCalledAfter(499);
 
   timeout.addTimeout({ milliseconds: 500 });
-  expect(callback).toHaveBeenCalledTimes(0);
-
-  jest.advanceTimersByTime(1);
-  expect(callback).toHaveBeenCalledTimes(1);
+  testCallbackIsCalledAfter(1);
 });
 
 test("Callback is triggered instantly when adding due timeout while already running", (): void => {
   timeout.start();
 
   timeout.addTimeout({ milliseconds: 1000 });
-  jest.advanceTimersByTime(501);
-  expect(callback).toHaveBeenCalledTimes(0);
-
+  testCallbackIsNotCalledAfter(501);
   timeout.addTimeout({ milliseconds: 500 });
   expect(callback).toHaveBeenCalledTimes(1);
 });
